b269ee58e64c2b0326f192ac32ccdc4ca6a3389e
nand.kishore <nand.kishore@coindcx.com>
2024-05-22 10:38:37 +0530

    Initial Commit

---
+++ PokemonApp/PokemonAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokemonAppApp.swift
+//  PokemonApp
+//
+//  Created by Nand Kishore on 22/05/24.
+//
+
+import SwiftUI
+
+@main
+struct PokemonAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
